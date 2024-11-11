ce22a79c96c2d3d3c94542de0befb94f96a6530c
Alexander2198 <112416461+Alexander2198@users.noreply.github.com>
2024-11-11 17:23:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,5 +8,6 @@ def hello_world():
     return 'Hola, Mundo :D'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    # Usa el puerto desde la variable de entorno PORT, o 5000 si no está definida
+    port = int(os.environ.get("PORT", 5000))  # Railway pasa la variable PORT
+    app.run(host='0.0.0.0', port=port)
